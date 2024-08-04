f2954d115bef9984874335a67c98989f26fc7df3
Mihail Samofalov <misasamofalov611@gmail.com>
2024-08-04 14:52:28 +0700

    переписал сервис - прослойку для моделей, изменил API

--- src/services/person.py
+++ src/services/person.py
@@ -1,39 +1,5 @@
-from models.person import Person
-from structs.person import PersonStruct, CreatePersonStruct, UpdatePersonStruct
+from services.base_crud import BaseCRUD
 
 
-class PersonService:
-    @classmethod
-    def get_person_by_id(cls, person_id: int, db) -> PersonStruct:
-        query = db.query(Person).filter(Person.id == person_id).first()
-        data = PersonStruct.model_validate(query)
-        return data
-
-    @classmethod
-    def get_all_person(cls, db) -> list[PersonStruct]:
-        query = db.query(Person).all()
-        data = [PersonStruct.model_validate(el) for el in query]
-        return data
-
-    @classmethod
-    def create_person(cls, data: CreatePersonStruct, db) -> PersonStruct:
-        person = Person(**data.model_dump())
-        db.add(person)
-        db.commit()
-        db.refresh(person)
-        return PersonStruct.model_validate(person)
-
-    @classmethod
-    def update_person(cls, person_id: int, data: UpdatePersonStruct, db) -> PersonStruct:
-        person = db.query(Person).filter(Person.id == person_id).first()
-        for key, value in data.dict.items():
-            setattr(person, key, value)
-        db.commit()
-        db.refresh(person)
-        return PersonStruct.model_validate(person)
-
-    @classmethod
-    def delete_person(cls, person_id: int, db):
-        db.query(Person).filter(Person.id == person_id).delete()
-        db.commit()
-        return {'message': 'Person deleted successfully'}
+class PersonService(BaseCRUD):
+    pass
--- src/services/task.py
+++ src/services/task.py
@@ -1,41 +1,5 @@
-from models.task import Task
-from structs.task import TaskStruct, CreateTaskStruct, UpdateTaskStruct
+from services.base_crud import BaseCRUD
 
 
-class TaskService:
-    @classmethod
-    def get_all_tasks(cls, db) -> list[TaskStruct]:
-        query = db.query(Task).all()
-        data = [TaskStruct.model_validate(el) for el in query]
-        return data
-
-    @classmethod
-    def get_task_by_id(cls, task_id: int, db) -> TaskStruct:
-        query = db.query(Task).filter(Task.id == task_id).first()
-        data = TaskStruct.model_validate(query)
-        return data
-
-    @classmethod
-    def create_task(cls, data: CreateTaskStruct, db) -> TaskStruct:
-        task = Task(**data.model_dump())
-        db.add(Task)
-        db.commit()
-        db.refresh(task)
-        return TaskStruct.model_validate(task)
-
-    @classmethod
-    def update_task(cls, task_id: int, data: UpdateTaskStruct, db) -> TaskStruct:
-        task = db.query(Task).filter(Task.id == task_id).first()
-        for key, value in data.dict.items():
-            setattr(task, key, value)
-        db.commit()
-        db.refresh(task)
-        return TaskStruct.model_validate(TaskStruct)
-
-    @classmethod
-    def delete_task(cls, task_id: int, db):
-        db.query(Task).filter(Task.id == task_id).delete()
-        db.commit()
-        return {'message': 'Task deleted successfully'}
-
-
+class TaskService(BaseCRUD):
+    pass
